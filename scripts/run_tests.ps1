c2798d5fc54f27756c245088bcdcfb092f4df24d
Keith Amundson <keith.amundson@wolterskluwer.com>
2021-07-23 14:55:40 -0500

    remove cloning and installing

--- scripts/run_tests.ps1
+++ scripts/run_tests.ps1
@@ -1,5 +1,2 @@
-﻿git clone https://github.com/kmamundson/django.git django-repo
-cd django-repo\tests
-py -m pip install -e ..
-py -m pip install -r requirements\py3.txt
+﻿cd tests
 runtests.py --settings=path.to.settings i18n.tests.TranslationTests.test_lazy_objects
