498c7cf8fc49a8d3e05b6ce09a235c1af0345519
Keith Amundson <keith.amundson@wolterskluwer.com>
2021-07-23 14:51:15 -0500

    run one test

--- scripts/run_tests.ps1
+++ scripts/run_tests.ps1
@@ -2,4 +2,4 @@
 cd django-repo\tests
 py -m pip install -e ..
 py -m pip install -r requirements\py3.txt
-runtests.py --settings=path.to.settings generic_relations i18n
+runtests.py --settings=path.to.settings i18n.tests.TranslationTests.test_lazy_objects
